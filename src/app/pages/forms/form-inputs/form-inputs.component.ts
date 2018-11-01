8e2c86f6d65ace2df285a50ce3b771be58e3d59e
MoisesAK <moises.saifell@gmail.com>
2018-11-01 17:26:05 -0300

    alterei um monte de coisa :D

--- src/app/pages/forms/form-inputs/form-inputs.component.ts
+++ src/app/pages/forms/form-inputs/form-inputs.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, NgModule} from '@angular/core';
+
+
+
 
 @Component({
   selector: 'ngx-form-inputs',
-  styleUrls: ['./form-inputs.component.scss'],
+  styleUrls: ['./form-inputs.component.scss',
+    '../../ui-features/buttons/hero-buttons/hero-buttons.component.scss'],
   templateUrl: './form-inputs.component.html',
+})
+
+@NgModule({
+  imports: [
+  ],
 })
 export class FormInputsComponent {
 
