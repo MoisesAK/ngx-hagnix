48d710874c8f9151ec55261f9ae1b1a397d80624
MoisesAK <moises.saifell@gmail.com>
2018-11-13 08:49:08 -0200

    pagina do game

---
+++ src/app/pages/game-page/game-page.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {link} from "fs";
+import {Router} from "@angular/router";
+
+
+@Component({
+  selector: 'ngx-game-page',
+  templateUrl: './game-page.component.html',
+  styleUrls: ['./game-page.component.scss']
+})
+export class GamePageComponent implements OnInit {
+
+  logged = localStorage.getItem('currentUser');
+
+  constructor(private router: Router) {
+  }
+
+  ngOnInit() {
+    const div = document.getElementById("nb-global-spinner")
+    console.log(div)
+    if (div) {
+      div.style['display'] = 'none';
+    }
+  }
+
+}
